9297403dfb9b893eab872625a95401c31733a584
nish33 <28823490+nish33@users.noreply.github.com>
2023-10-03 12:30:21 +0545

    minor changes

--- javascript/marksheet/script.js
+++ javascript/marksheet/script.js
@@ -1,58 +1,61 @@
 const marks = [
-    {
-        name: 'ram',
-        english: 50,
-        math: 60,
-        nepali: 70,
-        science: 40,
-        social: 30
+  {
+    name: "ram",
+    english: 50,
+    math: 60,
+    nepali: 70,
+    science: 40,
+    social: 30,
+    total: function () {
+      return this.english;
     },
-    {
-        name: 'hari',
-        english: 80,
-        math: 60,
-        nepali: 70,
-        science: 70,
-        social: 50
-    },
-    {
-        name: 'sita',
-        english: 70,
-        math: 68,
-        nepali: 72,
-        science: 39,
-        social: 58
-    },
-    {
-        name: 'gita',
-        english: 90,
-        math: 82,
-        nepali: 78,
-        science: 77,
-        social: 82
-    }
+  },
+  {
+    name: "hari",
+    english: 80,
+    math: 60,
+    nepali: 70,
+    science: 70,
+    social: 50,
+  },
+  {
+    name: "sita",
+    english: 70,
+    math: 68,
+    nepali: 72,
+    science: 39,
+    social: 58,
+  },
+  {
+    name: "gita",
+    english: 90,
+    math: 82,
+    nepali: 78,
+    science: 77,
+    social: 82,
+  },
 ];
 
 const container = document.getElementById("result");
 
 marks.forEach((el, index) => {
-    let result = "Pass";
-    for(mark in el) {
-        if(el[mark] < 40) {
-            result = "Fail";
-            break;
-        }
+  let result = "Pass";
+  for (mark in el) {
+    if (el[mark] < 40) {
+      result = "Fail";
+      break;
     }
-    container.innerHTML += `
-        <tr>
-            <td>${index+1}</td>
-            <td>${el.name}</td>
-            <td ${el.english < 40 && 'style="color: red;"'}>${el.english}</td>
-            <td ${el.math < 40 && 'style="color: red;"'}>${el.math}</td>
-            <td ${el.nepali < 40 && 'style="color: red;"'}>${el.nepali}</td>
-            <td ${el.science < 40 && 'style="color: red;"'}>${el.science}</td>
-            <td ${el.social < 40 && 'style="color: red;"'}>${el.social}</td>
-            <td ${result === "Fail" && 'style="color: red;"'}>${result}</td>
-        </tr>
-    `;
+  }
+  const tableRow = document.createElement("tr");
+  tableRow.innerHTML = `
+    <td>${index + 1}</td>
+    <td>${el.name}</td>
+    <td ${el.english < 40 && 'style="color: red;"'}>${el.english}</td>
+    <td ${el.math < 40 && 'style="color: red;"'}>${el.math}</td>
+    <td ${el.nepali < 40 && 'style="color: red;"'}>${el.nepali}</td>
+    <td ${el.science < 40 && 'style="color: red;"'}>${el.science}</td>
+    <td ${el.social < 40 && 'style="color: red;"'}>${el.social}</td>
+    <td ${result === "Fail" && 'style="color: red;"'}>${result}</td>
+  `;
+  container.appendChild(tableRow);
 });
